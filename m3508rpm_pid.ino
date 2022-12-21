99aacc0f4773cfa14ee8ce5642d01007a6f85a95
mii <109723126+MiiGoto@users.noreply.github.com>
2022-12-22 00:30:57 +0900

    Update m3508rpm_pid.ino

--- m3508rpm_pid.ino
+++ m3508rpm_pid.ino
@@ -14,6 +14,9 @@ CAN_message_t msg;//can送信用buf
 wheelEscDataSt wEscData[4];//can受信用
 
 Pid pid0;
+Pid pid1;
+Pid pid2;
+Pid pid3;
 
 void setup(void)
 {
@@ -27,36 +30,50 @@ void setup(void)
     msg.buf[idx] = 0;
   }
 
-  pid0.init(1.0, 0.0, 0.0);
+  pid0.init(3.0, 0.001, 0.003); //p,i,dの順に指定できる
+  pid1.init(3.0, 0.001, 0.003);
+  pid2.init(3.0, 0.001, 0.003);
+  pid3.init(3.0, 0.001, 0.003);
 
   MsTimer2::set(2, timerInt);
   MsTimer2::start();
 
 }
-
 int cnt=0;
 
 void loop(void)
 {
   int u[4] = {0};
-    if(cnt<200){
-     u[0]=1000;
-     Serial.print("1000");//目標速度
-    }
-    else{
-     u[0]=500;
-     Serial.print("500");//目標速度
-    }
-    cnt++;
-    if(cnt>400)cnt=0;
-    Serial.print(",");
-  u[0] = pid0.pid_out(u[0]);
+  u[0] = 500;
+  u[1] = 500;
+  u[2] = 500;
+  u[3] = 500; //ここの数字はrpm指定、-5000~5000くらい
+  
+  Serial.print(u[0]);//目標速度
+  Serial.print(",");
+  Serial.print(u[1]);
+  Serial.print(",");
+  Serial.print(u[2]);
+  Serial.print(",");
+  Serial.print(u[3]);
+  Serial.print(",");
 
+  u[0] = pid0.pid_out(u[0]);
+  u[1] = pid1.pid_out(u[1]);  
+  u[2] = pid2.pid_out(u[2]);
+  u[3] = pid3.pid_out(u[3]);
+  
   for (int i = 0; i < 4; i++) {
     msg.buf[i * 2] = u[i] >> 8;
     msg.buf[i * 2 + 1] = u[i] & 0xFF;
   }
   Serial.print(pid0.debug());//現在速度
+  Serial.println("");
+  Serial.print(pid1.debug());
+  Serial.println("");
+  Serial.print(pid2.debug());  
+  Serial.println("");
+  Serial.print(pid3.debug());
   Serial.println("");
   delay(10);
 }
@@ -64,8 +81,17 @@ void loop(void)
 void timerInt() {
   while ( CANTransmitter.read(rxmsg) ) {
     if (rxmsg.id == 0x201) {
-      pid0.now_value(rxmsg.buf[2] * 256 + rxmsg.buf[3]);
+      pid0.now_value(rxmsg.buf[0] * 256 + rxmsg.buf[1]);
+    }    
+    if (rxmsg.id == 0x202) {
+      pid1.now_value(rxmsg.buf[2] * 256 + rxmsg.buf[3]);
+    } 
+    if (rxmsg.id == 0x203) {
+      pid2.now_value(rxmsg.buf[4] * 256 + rxmsg.buf[5]);
     }
-  }
+    if (rxmsg.id == 0x204) {
+      pid3.now_value(rxmsg.buf[6] * 256 + rxmsg.buf[7]);
+    }
+     }
   CANTransmitter.write(msg);
 }
